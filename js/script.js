9a6aa020d3f50884d78a1d930d7aeeedab5d7c95
13reath <new13reath@gmail.com>
2023-02-08 11:17:30 +0200

    testing

--- js/script.js
+++ js/script.js
@@ -6,7 +6,43 @@ let queuedImagesArray = [],
     input = document.querySelector('.input-div input');
 
 // Queued Images
+
+const displayQueuedImages = () => {
+    let images = '';
+    queuedImagesArray.forEach((image, index) => {
+        images += `
+        <div class="image">
+            <img src="${URL.createObjectURL(image)}" alt="image" />
+            <span style="color:black" onclick="deleteQueuedImage(${index})">&times;</span>
+        </div>
+        `;
+    });
+
+    queuedDiv.innerHTML = images;
+};
+
+const deleteQueuedImage = (index) => {
+    queuedImagesArray.splice(index, 1);
+    displayQueuedImages();
+};
+
 input.addEventListener('change', () => {
     const files = input.files;
-    console.log(files);
+    for (let i = 0; i < files.length; i++) {
+        queuedImagesArray.push(files[i]);
+    }
+
+    queuedForm.reset();
+    displayQueuedImages();
+});
+
+inputDiv.addEventListener('drop', (e) => {
+    e.preventDefault();
+    const files = e.dataTransfer.files;
+    for (let i = 0; i < files.length; i++) {
+        if (!files[i].type.match('image')) return;
+        if (queuedImagesArray.every((image) => image.name !== files[i].name))
+            queuedImagesArray.push(files[i]);
+    }
+    displayQueuedImages();
 });
